9a44f47752f88e796020a1fe4c7c6b609f430648
Kévin Cadinot <kev.cadinot@gmail.com>
2017-02-05 10:01:01 +0100

    add ssh

--- bash_init.sh
+++ bash_init.sh
@@ -1,25 +1,65 @@
-#nom du client
+# Customer's name
 client_name="user"
 
-echo "Installation of the packages..."
+# Customer's mail address
+customer_mail="v.tarlowski@gmail.com"
+
+# Installing packages
+echo "Installation des packages"
 apt-get -y install ssh sudo vim rsync wget fail2ban 1> /dev/null
-echo "Done. "
 
-#ssh config
-echo "SSH-key..."
-#mv id_rsa.pub /home/$client_name/.ssh/authorized_keys
-echo " Added correctly."
-
-#modif root
-root_pass=$(date +%s | sha256sum | base64 | head -c 32 | tail -c 10)
+# Changing root's password
+root_pass=$(date +%s | sha256sum | head -c 32 | tail -c 10)
 passwd root $root_pass
 
-#creation user
+# ----------------------------------------- #
+# Changing to static IP adress - 172.16.2.* #
+# ----------------------------------------- #
+#echo "auto eth0
+#iface eth0 inet dhcp" >> /etc/network/interfaces;
+#/etc/init.d/networking restart
+ip_address=$(cat ip)
+#ip_address=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug eth2
+iface eth2 inet dhcp
+auto eth0
+iface eth0 inet static
+     address $ip_address
+     netmask 255.255.255.0
+     network 172.16.2.0
+     broadcast 172.16.2.255
+     gateway 172.16.2.254" > /etc/network/interfaces
+/etc/init.d/networking restart
+# ----------------------------------------- #
+
+# User creation
 pass=$(date +%s | sha256sum | base64 | head -c 32 | tail -c 10)
 useradd -m $client_name
 echo "$client_name:$pass" | chpasswd
 echo "pass = $pass
 user = $client_name"
+
+# SSH - Public keys configuration
+echo "SSH-key..."
+
+ssh_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWKB5iVi5l+LOTpKhb/8gqdPcAY7TCb27igZybUwzaVM0ajJsfbBZ8ECX5fo94kyqcPy+ULeIHgTkta8DARvm52NN3OT9tNSpxIXnnyTfGkjnpl8mw+qmmpBxxc/ADE4STEZ9m2sEjlNgHTvWVRKExSWfP+WefKlrJT7cRL9aXZGxVsuE66IIQQzgtSXT51oSgUX9t2MCRHlY5h38L9SPHEqZqbrXe0QgUfTOJvQh/1U/SGFiHL/tm+N7kZvyz5fwg7cY85npSUkJjqdyxPR2QM0Qq7jeHajryeAeVZDt3FWK/iDK16VbydVNzZoLyiKyH+hmvEAuPhkWVwY7ofR0N root@bad"
+
+mkdir /home/$client_name/.ssh
+touch /home/$client_name/.ssh/authorized_keys
+echo $ssh_key > /home/$client_name/.ssh/authorized_keys
+
+echo "Added correctly."
+
 
 #adresse mac
 echo "Hostname configuration.."
@@ -38,7 +78,10 @@ echo "User added to sudoers file."
 
 #### prompt
 echo "prompt is modifyng..."
-echo "PS1=\"\[\033[38;5;8m\][\[$(tput sgr0)\]\[\033[38;5;2m\]\u\[$(tput sgr0)\]\[\033[38;5;8m\]:\[$(tput sgr0)\]\[\033[38;5;117m\]\h\[$(tput sgr0)\]\[\033[38;5;8m\]-\[$(tput sgr0)\]\[\033[38;5;205m\]\W\[$(tput sgr0)\]\[\033[38;5;8m\]]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;7m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;208m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\"" >> /home/$client_name/.bashrc
+prompt='export PS1="\[\033[38;5;28m\]\u\[$(tput sgr0)\]\[\033[38;5;7m\]:\[$(tput sgr0)\]\[\033[38;5;39m\]\h\[$(tput sgr0)\]\[\033[38;5;7m\]-\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;39m\]\W\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;7m\]]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;7m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;214m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"'
+
+echo $prompt >> /etc/skel/.bashrc
+echo $prompt >> /etc/bash.bashrc
 
 #MOTD
 echo "MOTD download..."
@@ -51,29 +94,19 @@ echo "Done."
 
 #mail
 touch ~/.mail.txt~
-ip_adress=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 mac_adress=$(ifconfig | grep -i HWaddr | tail -c 20)
 
-echo "To: v.tarlowski@gmail.com                                                                                                                                                                                                                                                                                           
-Subject: Information about your Account                                                                                                                                                                                                                                                                                   
-From: assistancePXE@gmail.com                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-Hello,                                                                                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                                                                          
-First, thanks you to choice our PXE serveur.                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                                                                          
-You can read below your account Information for use your computer.                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                        
--adress IP = $ip_adress                                                                                                                                                                                                                                                                                                   
--adress MAC = $mac_adress                                                                                                                                                                                                                                                                                                 
--login = $client_name                                                                                                                                                                                                                                                                                                     
--password = $pass                                                                                                                                                                                                                                                                                                         
--Root password = $root_pass                                                                                                                                                                                                                                                                                                                         
--                                                                                                                                                                                                                                                                                                                         
--                                                                                                                                                                                                                                                                                                                        
--                                                                                                                                                                                                                                                                                                                        
--                                                                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                      
-See you soon.                                                                                                                                                                                                                                                                                                              
-" > ~/.mail.txt~
+echo "To: $customer_mail
+Subject: Information about your Debian Server
+From: assistancePXE@gmail.com
+
+Hello,
+
+You can read below the informations of your new Debian Server:
+- adress MAC = $mac_address
+- login = $client_name
+- password = $pass
+- Root password = $root_pass
+Thanks for using our service." > ~/.mail.txt~
 
 sendmail -vt < ~/.mail.txt~
